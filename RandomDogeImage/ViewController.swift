c0e88674bde80870fc88239425903033bc5f378b
Александр Пронин <demkadoo@gmail.com>
2021-11-27 15:41:15 +0300

    Initial Commit

---
+++ RandomDogeImage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomDogeImage
+//
+//  Created by Александр Пронин on 27.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
