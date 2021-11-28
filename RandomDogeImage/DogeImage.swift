7c0c4ad1190c2ec23c9c64afc349d4e91d8db16c
Александр Пронин <demkadoo@gmail.com>
2021-11-28 12:55:51 +0300

    Done

---
+++ RandomDogeImage/DogeImage.swift
@@ -0,0 +1,12 @@
+//
+//  DogeImage.swift
+//  RandomDogeImage
+//
+//  Created by Александр Пронин on 27.11.2021.
+//
+
+struct DogeImage: Decodable {
+    let fileSizeBytes: Int
+    let url: String
+}
+
--- RandomDogeImage/ViewController.swift
+++ RandomDogeImage/ViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet weak var dogeImageView: UIImageView!
+    
+    let link = "https://random.dog/woof.json"
+    
+    @IBAction func generateImageButton(_ sender: UIButton) {
+        
+        guard let url = URL(string: link) else { return }
+        
+        URLSession.shared.dataTask(with: url) { data , _ , error in
+            guard let data = data else { return }
+            
+            do {
+                let dogeImage = try JSONDecoder().decode(DogeImage.self, from: data)
+                
+                guard let url = URL(string: dogeImage.url) else {return}
+                guard let imageData = try? Data(contentsOf: url) else {return}
+                
+                DispatchQueue.main.async {
+                    self.dogeImageView.image = UIImage(data: imageData)
+                }
+            }
+            catch let error {
+                print(error.localizedDescription)
+            }
+        }.resume()
     }
-
-
 }
 
